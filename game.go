9a3fb26a9e2bbe0d7b1e56635bd66cd59d9e004d
caled529 <118690771+caled529@users.noreply.github.com>
2024-08-17 02:06:06 -0400

    Refactoring + minor optimizations

--- game.go
+++ game.go
@@ -1,61 +1,47 @@
 package main
 
-type Grid struct {
-	tileMap map[int]map[int]bool
-}
+type Grid map[int]map[int]bool
 
-func (g *Grid) IsAliveAt(x, y int) bool {
-	if g.tileMap[x][y] {
-		return true
-	}
-	return false
-}
-
-func (g *Grid) ToggleAt(x, y int) {
-	if g.tileMap[x][y] {
-		delete(g.tileMap[x], y)
-		if len(g.tileMap[x]) == 0 {
-			delete(g.tileMap, x)
-		}
-	} else if _, ok := g.tileMap[x]; ok {
-		g.tileMap[x][y] = true
-	} else {
-		g.tileMap[x] = make(map[int]bool)
-		g.tileMap[x][y] = true
-	}
-}
-
-func (g *Grid) NextGeneration() {
-	newMap := make(map[int]map[int]bool)
-	alive := [][2]int{}
-	for x, col := range g.tileMap {
+func (g Grid) NextState() (newGrid Grid) {
+	living := [][2]int{}
+	for x, col := range g {
 		for y := range col {
-			for i := x - 1; i < x+2; i++ {
-				for j := y - 1; j < y+2; j++ {
-					if g.tileMap[i][j] {
-						if n := g.neighboursAround(i, j); n == 2 || n == 3 {
-							alive = append(alive, [2]int{i, j})
-						}
-					} else if g.neighboursAround(i, j) == 3 {
-						alive = append(alive, [2]int{i, j})
-					}
-				}
+			if n := g.countNeighbours(x, y); n == 2 || n == 3 {
+				living = append(living, [2]int{x, y})
+			}
+			living = append(living, g.localNextState(x, y)...)
+		}
+	}
+	newGrid = make(map[int]map[int]bool)
+	for _, pos := range living {
+		if _, ok := newGrid[pos[0]]; !ok {
+			newGrid[pos[0]] = make(map[int]bool)
+		}
+	}
+	for _, pos := range living {
+		newGrid[pos[0]][pos[1]] = true
+	}
+	return newGrid
+}
+
+func (g Grid) localNextState(x, y int) (living [][2]int) {
+	for i := x - 1; i < x+2; i++ {
+		for j := y - 1; j < y+2; j++ {
+			if i == x && j == y {
+				continue
+			}
+			if !g[i][j] && g.countNeighbours(i, j) == 3 {
+				living = append(living, [2]int{i, j})
 			}
 		}
 	}
-	for _, pos := range alive {
-		newMap[pos[0]] = make(map[int]bool)
-	}
-	for _, pos := range alive {
-		newMap[pos[0]][pos[1]] = true
-	}
-	g.tileMap = newMap
+	return living
 }
 
-func (g *Grid) neighboursAround(x, y int) (n int) {
+func (g Grid) countNeighbours(x, y int) (n int) {
 	for i := x - 1; i < x+2; i++ {
 		for j := y - 1; j < y+2; j++ {
-			if g.tileMap[i][j] && (i != x || j != y) {
+			if g[i][j] && (i != x || j != y) {
 				n++
 			}
 		}
@@ -63,13 +49,34 @@ func (g *Grid) neighboursAround(x, y int) (n int) {
 	return n
 }
 
-func (g *Grid) BoundedView(x, y, width, height int) (s string) {
+func (g Grid) ToggleAt(x, y int) {
+	if g[x][y] {
+		delete(g[x], y)
+		if len(g[x]) == 0 {
+			delete(g, x)
+		}
+	} else if _, ok := g[x]; ok {
+		g[x][y] = true
+	} else {
+		g[x] = make(map[int]bool)
+		g[x][y] = true
+	}
+}
+
+func (g Grid) IsAlive(x, y int) bool {
+	if g[x][y] {
+		return true
+	}
+	return false
+}
+
+func (g Grid) BoundedView(x, y, width, height int) (s string) {
 	if height <= 0 || width <= 0 {
 		return ""
 	}
 	for j := range height {
 		for i := range width {
-			if _, ok := g.tileMap[x+i][y+j]; ok {
+			if _, ok := g[x+i][y+j]; ok {
 				s += "██"
 			} else {
 				s += "  "
