cfe786e970e29f4a3afcb590904f19007d686d6f
caled529 <118690771+caled529@users.noreply.github.com>
2024-09-09 13:06:15 -0400

    Moved package build to package.nix

--- package.nix
+++ package.nix
@@ -1,14 +1,14 @@
 {
   buildGoApplication,
-  fetchFromGithub,
-  ...
+  fetchFromGitHub,
 }:
 buildGoApplication {
   name = "seagoll";
-  src = fetchFromGithub {
+  src = fetchFromGitHub {
     owner = "caled529";
     repo = "seagoll";
     rev = "7c7b0bb6b4c7315ad24e46d17b86d10e32db62c3";
+    hash = "sha256-ZLhL7XKXxbxsWVXGPSNgHyd3baMRW31QopPBUumHSic=";
   };
   pwd = ./.;
 }
